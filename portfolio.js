f18bad7a5cdf7910286ce3e11db3cd26ffb4e920
Joao Victor <joao.araruna8@gmail.com>
2024-05-23 20:06:22 -0300

    responsividade e footer

--- portfolio.js
+++ portfolio.js
@@ -15,5 +15,12 @@ function curriculo() {
     var urlPDF = "curriculo-port.pdf";
     window.open(urlPDF, '_blank');
 }
+function github() {
+    var url = "https://github.com/jotaiwnl";
+    window.open(url, '_blank');
+}
 
-
+function linkedin() {
+    var url = "https://www.linkedin.com/in/joao-victor-8a976025b/";
+    window.open(url, '_blank');
+}
